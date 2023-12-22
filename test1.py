768b9e3ea55fd0a9be1383d0e42c426309f71d3a
mbimbimbimbi <153854323+mbimbimbimbi@users.noreply.github.com>
2023-12-22 15:01:41 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,17 +2,6 @@ import streamlit as st
 
 import time
 
-# İletişim koçunun(iletisimkocu) oluşturduğu cevabı buradan alıyoruz. Şimdilik random cevaplar ekledim, fakat buraya iletişim koçunun API'nı bağlamak gerekiyor
-def get_iletisimkocu_response(message):
-    responses = {
-        "hi": "Hello! How can I help you today?",
-        "how are you?": "I'm a bot, so I don't have feelings, but thanks for asking!",
-        "bye": "Goodbye! Have a great day!"
-    }
-    # Konu dışı konuları da buradan ayarlamamız gerekecek, fakat bunu iletisimkocundan mı yapmak gerek yoksa router agent'tan mı yapmak gerek bunu bir düşünelim belki buraya ilave bir katman gerekebilir
-    return responses.get(message.lower(), "I'm sorry, I don't understand that.")
-
-# Streamlit app layout
 def main():
 
     # Set the page to wide mode for better chat layout
@@ -49,3 +38,16 @@ def main():
 # Çalıştırma  
 if __name__ == "__main__":
     main() 
+
+
+# İletişim koçunun(iletisimkocu) oluşturduğu cevabı buradan alıyoruz. Şimdilik random cevaplar ekledim, fakat buraya iletişim koçunun API'nı bağlamak gerekiyor
+def get_iletisimkocu_response(message):
+    responses = {
+        "hi": "Hello! How can I help you today?",
+        "how are you?": "I'm a bot, so I don't have feelings, but thanks for asking!",
+        "bye": "Goodbye! Have a great day!"
+    }
+    # Konu dışı konuları da buradan ayarlamamız gerekecek, fakat bunu iletisimkocundan mı yapmak gerek yoksa router agent'tan mı yapmak gerek bunu bir düşünelim belki buraya ilave bir katman gerekebilir
+    return responses.get(message.lower(), "I'm sorry, I don't understand that.")
+
+# Streamlit app layout
