08bd4c400dac88ea01faf55b3b16a6d3d2edc02d
mbimbimbimbi <153854323+mbimbimbimbi@users.noreply.github.com>
2023-12-26 15:27:59 +0300

    test3.py

--- test3.py
+++ test3.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import time
 
-# Set the page config as the first thing in your script
+# This should be the first Streamlit command used
 st.set_page_config(page_title="Team J.A.R.V.I.S.", layout="wide")
 
+# Function to get the response from the chatbot
 def get_iletisimkocu_response(message):
     responses = {
         "hi": "Hello! How can I help you today?",
@@ -12,18 +13,17 @@ def get_iletisimkocu_response(message):
     }
     return responses.get(message.lower(), "I'm sorry, I don't understand that.")
 
-
+# Main function for the Streamlit app
 def main():
-    st.set_page_config(page_title="Team J.A.R.V.I.S.", layout="wide")
     st.title("Team J.A.R.V.I.S.")
 
-    # Initialize session state for chat history and user input
+    # Initialize chat history and user input in session state
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history = []
     if 'user_input' not in st.session_state:
         st.session_state.user_input = ""
 
-    # Display chat history
+    # Function to display the chat history
     def display_chat():
         for message in st.session_state.chat_history:
             st.text(message)
@@ -37,22 +37,23 @@ def main():
     with chat_container:
         display_chat()
 
-    # When user sends a message
+    # Handling user input
     if user_input:
         user_message = "Kullanici: " + user_input
         st.session_state.chat_history.append(user_message)
 
-        # Simulate typing
+        # Simulate typing delay
         with st.spinner("J.A.R.V.I.S. yaziyor..."):
             time.sleep(2)
 
-        # Get and display response
+        # Get and display bot response
         bot_response = "J.A.R.V.I.S.: " + get_iletisimkocu_response(user_input)
         st.session_state.chat_history.append(bot_response)
 
-        # Clear input box and update chat
+        # Clear input box and update chat history
         st.session_state.user_input = ""
         display_chat()
 
+# Run the main function
 if __name__ == "__main__":
     main()
