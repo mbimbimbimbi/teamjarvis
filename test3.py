3dc3b47eacb59a76cfd4f08110bcf1c7cd52aeac
mbimbimbimbi <153854323+mbimbimbimbi@users.noreply.github.com>
2023-12-26 15:42:51 +0300

    test3.py

--- test3.py
+++ test3.py
@@ -9,7 +9,8 @@ def get_iletisimkocu_response(message):
     responses = {
         "hi": "Hello! How can I help you today?",
         "how are you?": "I'm a bot, so I don't have feelings, but thanks for asking!",
-        "bye": "Goodbye! Have a great day!"
+        "bye": "Goodbye! Have a great day!",
+        "greetings": "Greetings! What can I do for you?"
     }
     return responses.get(message.lower(), "I'm sorry, I don't understand that.")
 
@@ -20,8 +21,6 @@ def main():
     # Initialize chat history and reset trigger in session state
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history = []
-    if 'reset_trigger' not in st.session_state:
-        st.session_state.reset_trigger = False
 
     # Function to display the chat history
     def display_chat():
@@ -34,30 +33,24 @@ def main():
         display_chat()
 
     # User input and submit button
-    user_input = st.text_input("Kullanıcı", placeholder="Lütfen sorunuzu yazınız", key="user_input", value="" if st.session_state.reset_trigger else None)
+    user_input = st.text_input("Kullanıcı", placeholder="Lütfen sorunuzu yazınız", key="user_input")
 
     submit_button = st.button("Send")
 
     # Handling user input when the button is clicked
     if submit_button and user_input:
         user_message = "Kullanici: " + user_input
-        st.session_state.chat_history.append(user_message)
-
-        # Simulate typing delay
-        with st.spinner("J.A.R.V.I.S. yaziyor..."):
-            time.sleep(2)
-
-        # Get and display bot response
         bot_response = "J.A.R.V.I.S.: " + get_iletisimkocu_response(user_input)
-        st.session_state.chat_history.append(bot_response)
 
-        # Set the reset trigger
-        st.session_state.reset_trigger = not st.session_state.reset_trigger
+        # Update chat history
+        st.session_state.chat_history.extend([user_message, bot_response])
+
+        # Clear the user input field
+        st.session_state.user_input = ""
 
         # Refresh the chat display
         with chat_container:
             display_chat()
 
 # Run the main function
-if __name__ == "__main__":
-    main()
+if __name__ == 
